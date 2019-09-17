e7b10934fa27772f012245e62fd4620866b077a0
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-09-17 23:28:16 +0100

    Changed colour scheme -> RWC Scheme

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -4,9 +4,9 @@ function renderHeader(){
     for (let i of tags) {
         i.innerHTML = `
         <div class="row d-flex justify-content-between mx-5">
-        <i class="material-icons header-icon">sports_rugby</i>
-           <h1 class='header-brand'>Rugby World Cup 2019</h1>
-        <i class="material-icons header-icon">sports_rugby</i>
+            <image class="rwc-logo" src="./img/Teams/RWC_logo.png">
+            <h1 class='header-brand'>Rugby World Cup 2019</h1>
+            <image class="rwc-logo" src="./img/Teams/RWC_logo.png">
         </div>
         `;
     }
