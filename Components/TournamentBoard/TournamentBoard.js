9d4e08988797e1afa164ab61995440dbd335826e
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-09-18 18:54:46 +0100

    Tournament grid refactor

--- Components/TournamentBoard/TournamentBoard.js
+++ Components/TournamentBoard/TournamentBoard.js
@@ -3,59 +3,36 @@ function renderTournamentBoard(){
     for (let i of tags) {
 
         let HTML = `
-        <div class="row">
-            <div class="col">
-                <div class="TournamentTable" Pool="A"></div>
-            </div>
-            <div class="col">
-                <div class="TournamentTable" Pool="B"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentTable" Pool="A"></div>
+            <div class="TournamentTable" Pool="B"></div>
         </div>
 
-        <div class="row">
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool A" namePlayer2="Runner-Up Pool B"></div>
-            </div>
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool B" namePlayer2="Runner-Up Pool A"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool A" namePlayer2="Runner-Up Pool B"></div>
+            <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool B" namePlayer2="Runner-Up Pool A"></div>
         </div>
 
-        <div class="row">
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Semi Final" namePlayer1="Winner QF1" namePlayer2="Winner QF2"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentKnockoutMatch" RoundLabel="Semi Final" namePlayer1="Winner QF1" namePlayer2="Winner QF2"></div>
         </div>
 
-        <div class="row">
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Final" namePlayer1="Winner SF1" namePlayer2="Winner SF2"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentKnockoutMatch" RoundLabel="Final" namePlayer1="Winner SF1" namePlayer2="Winner SF2"></div>
         </div>
 
-        <div class="row">
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Semi Final" namePlayer1="Winner QF3" namePlayer2="Winner QF4"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentKnockoutMatch" RoundLabel="Semi Final" namePlayer1="Winner QF3" namePlayer2="Winner QF4"></div>
         </div>
 
-
-        <div class="row">
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool C" namePlayer2="Runner-Up Pool D"></div>
-            </div>
-            <div class="col">
-                <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool D" namePlayer2="Runner-Up Pool C"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool C" namePlayer2="Runner-Up Pool D"></div>
+            <div class="TournamentKnockoutMatch" RoundLabel="Quarter Final" namePlayer1="Winner Pool D" namePlayer2="Runner-Up Pool C"></div>
         </div>
 
-        <div class="row">
-            <div class="col">
-                <div class="TournamentTable" Pool="C"></div>
-            </div>
-            <div class="col">
-                <div class="TournamentTable" Pool="D"></div>
-            </div>
+        <div class="tournament-grid-container">
+            <div class="TournamentTable" Pool="C"></div>
+            <div class="TournamentTable" Pool="D"></div>
         </div>
 
         `;
