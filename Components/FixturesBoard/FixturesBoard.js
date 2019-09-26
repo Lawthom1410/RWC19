d2bd2c5f2e1dba7cd27280f320586741eb534de2
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-09-26 11:50:52 +0100

    Fixtures/Results filters

--- Components/FixturesBoard/FixturesBoard.js
+++ Components/FixturesBoard/FixturesBoard.js
@@ -1,19 +1,36 @@
-function renderFixturesBoard(){
-    tags = document.getElementsByClassName("FixturesBoard");
-    for (let i of tags) {
-      let currentDate;
-        
-      let HTML = ``;
-      for (let fixture of FIXTURES){
-        if (currentDate != fixture['date']){
-          currentDate = fixture['date'];
-          HTML += `
-          <div class="FixturesDateContainer" Date="`+currentDate+`"></div>
-          `
-        }
+function renderFixturesBoard(activeMatches){
+  if(!activeMatches) {activeMatches = FIXTURES}
+  tags = document.getElementsByClassName("FixturesBoard");
+  for (let i of tags) {
+    let currentDate;
+      
+    let HTML = `
+    <div>
+      <button class="btn btn-lg fixtures-filter-btn" onClick="renderFixturesBoard()">All</button>
+      <button class="btn btn-lg fixtures-filter-btn" onClick="filterFixtures()">Fixtures</button>
+      <button class="btn btn-lg fixtures-filter-btn" onClick="filterResults()">Results</button>
+    </div>
+    `;
+    for (let match of activeMatches){
+      if (currentDate != match['date']){
+        currentDate = match['date'];
+        HTML += `
+        <div class="FixturesDateContainer" Date="`+currentDate+`"></div>
+        `
       }
-
-      i.innerHTML = HTML;
     }
-    renderFixturesDateContainer();
+
+    i.innerHTML = HTML;
+  }
+  renderFixturesDateContainer(activeMatches);
+}
+
+function filterFixtures() {
+  activeMatches = FIXTURES.filter(match => match['homeScore']==null);
+  renderFixturesBoard(activeMatches);
+}
+
+function filterResults() {
+  activeMatches = FIXTURES.filter(match => match['homeScore']!=null);
+  renderFixturesBoard(activeMatches);
 }
--- Components/FixturesDateContainer/FixturesDateContainer.js
+++ Components/FixturesDateContainer/FixturesDateContainer.js
@@ -1,9 +1,9 @@
-function renderFixturesDateContainer(){
+function renderFixturesDateContainer(activeMatches){
+    if(!activeMatches) {activeMatches = FIXTURES}
     tags = document.getElementsByClassName("FixturesDateContainer");
     for (let i of tags) {
         let date = i.getAttribute("Date");
-
-        let dateFixtures = FIXTURES.filter(fixture => fixture['date']==date);
+        let dateFixtures = activeMatches.filter(fixture => fixture['date']==date);
 
         let HTML = `<h1 class="date-label">`+date.toUpperCase()+`</h1>`;
         for (fixture of dateFixtures){
